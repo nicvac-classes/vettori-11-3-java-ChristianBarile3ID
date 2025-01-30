6e658611b3b77d06a44ad69be2ac8f8dc3cf50f8
ChristianBarile3ID <christianalessandro.barile@marconibari.edu.it>
2025-01-30 15:08:44 +0000

    consegna

--- Esercizio.java
+++ Esercizio.java
@@ -3,16 +3,60 @@ import java.util.Arrays;
 
 class Esercizio {
 
-    public static void eliminaDuplicati(int[] V) {
-
+    public static int eliminaDuplicati(int[] V) {
+        int i, j, k, N2, h;
+        int[] W = new int [V.length-1];
+        N2 = V.length-1;
+        for (i=0; i<=V.length-2; i++) {
+            for (j=i+1; j<=V.length-1; j++) {
+                if (V[i]==V[j]) {
+                    k = j;
+                    for (h=0; h<=k-1; h++) {
+                        W[i] = V[i];
+                    }
+                    for (h=k; h<=V.length-1; h++) {
+                        W[i] = V[i+1];
+                    }
+                    for (h=0; h<=V.length-1; h++) {
+                        V[i] = W[i];
+                    }
+                }
+            }
+        }
+        return N2;
     }
 
-    public static void eliminaElemento(int[] V, int posizione) {
-
+    public static int eliminaElemento(int[] V, int posizione) {
+        int i, N2;
+        int[] W = new int[V.length-1];
+        N2 = V.length-1;
+        for (i=0; i<=posizione-1; i++) {
+            W[i] = V[i];
+        }
+        for (i=posizione; i<=V.length-1; i++) {
+            W[i] = V[i+1];
+        }
+        for (i=0; i<=V.length-1; i++) {
+            V[i] = W[i];
+        }
+        return N2;
     }
 
-    public static void inserisciElemento(int[] V, int posizione, int valore) {
-
+    public static int inserisciElemento(int[] V, int posizione, int valore) {
+        int i, N2;
+        int[] W = new int[V.length+1];
+        N2 = V.length+1;
+        for (i=0; i<=posizione-1; i++) {
+            W[i] = V[i];
+        }
+        W[posizione] = valore;
+        for (i=posizione; i<=V.length-1; i++) {
+            W[i] = V[i-1];
+        }
+        for (i=0; i<=V.length; i++) {
+            V[i] = W[i];
+        }
+        return N2;
     }
 
     public static int leggiComando() {
@@ -33,7 +77,13 @@ class Esercizio {
     }
 
     public static int ricercaElemento(int[] V, int valore) {
-        int indice = 0;
+        int i, indice;
+        indice = -1;
+        for (i=0; i<=V.length; i++) {
+            if (V[i]==valore) {
+                indice = i;
+            }
+        }
         return indice;
     }
 
@@ -47,7 +97,7 @@ class Esercizio {
     public static void main(String args[]) { 
 
         Scanner in = new Scanner( System.in );
-        int N, pos, x, scelta;
+        int N, pos, x, i, scelta, indice;
         System.out.println("Inserire la grandezza del vettore: ");
         N = Integer.parseInt(in.nextLine());
         int[] V = new int[N];
@@ -56,24 +106,35 @@ class Esercizio {
             scelta = leggiComando();
             switch (scelta) {
             case 1:
+                do {
+                    System.out.println("Inserire la posizione: ");
+                    pos = Integer.parseInt(in.nextLine());
+                } while(pos<0 || pos>V.length-1);
                 System.out.println("Inserire la posizione: ");
                 pos = Integer.parseInt(in.nextLine());
                 System.out.println("Inserire il valore: ");
                 x = Integer.parseInt(in.nextLine());
-                inserisciElemento(V, pos, x);
+                N = inserisciElemento(V, pos, x);
                 break;
             case 2:
-                System.out.println("Inserire la posizione: ");
-                pos = Integer.parseInt(in.nextLine());
-                eliminaElemento(V, pos);
+                do {
+                    System.out.println("Inserire la posizione: ");
+                    pos = Integer.parseInt(in.nextLine());
+                } while(pos<0 || pos>V.length-1); 
+                N =  eliminaElemento(V, pos);
                 break;
             case 3:
                 System.out.println("Inserire il valore: ");
                 x = Integer.parseInt(in.nextLine());
-                ricercaElemento(V, x);
+                indice = ricercaElemento(V, x);
+                if (indice>=0) {
+                    System.out.println("Elemento: " + x + ", trovato in posizione: " + indice);
+                } else {
+                    System.out.println("Elemento: " + x + " non trovato");
+                }
                 break;
             case 4:
-                eliminaDuplicati(V);
+                N = eliminaDuplicati(V);
                 break;
             case 5:
                 visualizzaVettore(V);
